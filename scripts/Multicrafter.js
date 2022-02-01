21977f9fa99931d1906f5fc19870c4df8426aefb
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-01 18:22:05 +0100

    added arc furnace

--- scripts/Multicrafter.js
+++ scripts/Multicrafter.js
@@ -205,3 +205,115 @@ inductionFurnace.health = 500;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 inductionFurnace.dumpToggle = false;
+
+const electricArcFurnace = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "electric-arc-furnace", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["altanic-ironore/1"],
+            power: 2,
+        },
+        output: {
+            items: ["altanic-zelazo/1"],
+        },
+        craftTime: 7.5
+    },
+    {
+        input: {
+            items: ["coal/1", "sand/2"],
+            power: 2,
+        },
+        output: {
+            items: ["silicon/1"],
+        },
+        craftTime: 7.5
+    },
+    {
+        input: {
+            items: ["coal/2", "altanic-ironore/1"],
+            power: 6,
+        },
+        output: {
+            items: ["altanic-stal/1"],
+        },
+        craftTime: 25
+    },
+    {
+        input: {
+            items: ["lead/1", "sand/1"],
+            power: 2,
+        },
+        output: {
+            items: ["metaglass/1"],
+        },
+        craftTime: 7.5
+    },
+    {
+        input: {
+            items: ["coal/5"],
+            power: 4,
+        },
+        output: {
+            items: ["altanic-cokecoal/5"],
+        },
+        craftTime: 30
+    },
+        {
+        input: {
+            items: ["altanic-cokecoal/5", "altanic-ironore/5"],
+            power: 12,
+        },
+        output: {
+            items: ["altanic-stal/5"],
+            liquids: ["slag/1.5"],
+        },
+        craftTime: 30
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
+electricArcFurnace.localizedName = "Electric arc furnace";
+electricArcFurnace.description = "Electric arc furnace is a masive structure that requires lots of power to work, but is very fast";
+electricArcFurnace.itemCapacity = 20;
+electricArcFurnace.size = 4;
+electricArcFurnace.health = 1000;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+electricArcFurnace.dumpToggle = false;
