fbc5cdc279796c7cc3465f6db813c862aebfbc06
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-01 17:44:52 +0100

    Added induction smelter

--- scripts/Multicrafter.js
+++ scripts/Multicrafter.js
@@ -86,7 +86,122 @@ furnace.localizedName = "Electric Furnace";
 furnace.description = "Electir Furnace use power to smelt items";
 furnace.itemCapacity = 20;
 furnace.size = 2;
-furnace.health = 100;
+furnace.health = 250;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 furnace.dumpToggle = false;
+
+
+
+const inductionFurnace = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "induction-furnace", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["altanic-ironore/1"],
+            power: 1,
+        },
+        output: {
+            items: ["altanic-zelazo/1"],
+        },
+        craftTime: 15
+    },
+    {
+        input: {
+            items: ["coal/1", "sand/2"],
+            power: 1,
+        },
+        output: {
+            items: ["silicon/1"],
+        },
+        craftTime: 15
+    },
+    {
+        input: {
+            items: ["coal/2", "altanic-ironore/1"],
+            power: 3,
+        },
+        output: {
+            items: ["altanic-stal/1"],
+        },
+        craftTime: 45
+    },
+    {
+        input: {
+            items: ["lead/1", "sand/1"],
+            power: 1,
+        },
+        output: {
+            items: ["metaglass/1"],
+        },
+        craftTime: 15
+    },
+    {
+        input: {
+            items: ["coal/5"],
+            power: 2,
+        },
+        output: {
+            items: ["altanic-cokecoal/5"],
+        },
+        craftTime: 60
+    },
+        {
+        input: {
+            items: ["altanic-cokecoal/5", "altanic-ironore/5"],
+            power: 6,
+        },
+        output: {
+            items: ["altanic-stal/5"],
+            liquids: ["slag/1"],
+        },
+        craftTime: 60
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
+inductionFurnace.localizedName = "Induction furnce";
+inductionFurnace.description = "Induction smelter use power to smelt even faster, and also can use more advanced metalurgic proces";
+inductionFurnace.itemCapacity = 40;
+inductionFurnace.liquidCapacity = 20;
+inductionFurnace.size = 2;
+inductionFurnace.health = 500;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+inductionFurnace.dumpToggle = false;
