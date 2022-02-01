dc978e66f55748637978b1039fb3f8c720f302cd
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-01 13:42:06 +0100

    Add files via upload

--- scripts/Multicrafter.js
+++ scripts/Multicrafter.js
@@ -44,9 +44,24 @@ const furnace = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCraf
         },
         craftTime: 30
     },
-], /*{
-    you can customize block here. ex) load()
-},*/
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
 /*
 YOU MUST NOT MODIFY VALUE OF THESE
 configurable
@@ -56,12 +71,12 @@ hasLiquids
 hasPower
 */
 //using example without .json file. I don't recommand this way because you can't use mod item as requirements.
-multi.localizedName = "Electric Furnace":
-multi.description = "Electric use power to smelt items";
-multi.itemCapacity = 20;
-multi.size = 2;
-multi.health = 100;
+furnace.localizedName = "Electric Furnace":
+furnace.description = "Electric use power to smelt items";
+furnace.itemCapacity = 20;
+furnace.size = 2;
+furnace.health = 100;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
-multi.dumpToggle = false;
-multi.requirements(Category.crafting, ItemStack.with(Items.copper, 75));
+furnace.dumpToggle = false;
+furnace.requirements(Category.crafting, ItemStack.with(Items.copper, 75, lead, 65, graphite, 50, metaglass, 30, zelazo, 55));
