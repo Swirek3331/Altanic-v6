f7700fe3b34e10f1b8aefa158a2510459dedcd75
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-02 11:02:08 +0100

    added macerator

--- scripts/Multicrafter.js
+++ scripts/Multicrafter.js
@@ -317,3 +317,84 @@ electricArcFurnace.health = 1000;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 electricArcFurnace.dumpToggle = false;
+
+const macerator = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "macerator", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["scrap"],
+            power: 0.4
+        },
+        output: {
+            items: ["sand/2"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["scrap"],
+            power: 0.4
+        },
+        output: {
+            items: ["altani-ironore/2"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["sand/2"],
+            power: 0.5
+        },
+        output: {
+            items: ["quartz/2"],
+        },
+        craftTime: 60
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
+macerator.localizedName = "Macerator";
+macerator.description = "Macerator";
+macerator.itemCapacity = 20;
+macerator.size = 2;
+macerator.health = 250;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+macerator.dumpToggle = false;
