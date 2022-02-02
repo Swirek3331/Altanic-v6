e46e598ef0105dd916da5922e476317a71a45599
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-02 11:18:33 +0100

    Create Attributes.js

---
+++ scripts/Attributes.js
@@ -0,0 +1,7 @@
+Block.stone.attributes = set(Attribute.stone, 1f);
+Block.craters.attributes = set(Attribute.stone, 1f);
+Block.charr.attributes = set(Attribute.stone, 1.2f);
+Block.dacite.attributes = set(Attribute.stone, 1f);
+Block.basalt.attributes = set(Attribute.stone, 1.5f);
+Block.shale.attributes = set(Attribute.stone, 1.75f);
+//I don't think i will be work
