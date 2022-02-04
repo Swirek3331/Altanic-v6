d508ee05d18bf63c43b82e99874108421960343f
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-04 13:08:19 +0100

    Update Multicrafter.js

--- scripts/Multicrafter.js
+++ scripts/Multicrafter.js
@@ -398,3 +398,74 @@ macerator.health = 250;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 macerator.dumpToggle = false;
+
+const thermal-centrifuge = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "thermal-centrifuge", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["altanic-thorium-waste/5"],
+            power: 5,
+        },
+        output: {
+            items: ["thorium/2", "altanic-uranium/4"],
+        },
+        craftTime: 120
+    },
+    {
+        input: {
+            items: ["altanic-uranium-waste/5"],
+            power: 5,
+        },
+        output: {
+            items: ["altanic-uranium/2", "plutonium/4"],
+        },
+        craftTime: 120
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
+thermal-centrifuge.localizedName = "thermal-centrifuge";
+thermal-centrifuge.description = "thermal-centrifuge";
+thermal-centrifuge.itemCapacity = 30;
+thermal-centrifuge.size = 3;
+thermal-centrifuge.health = 750;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+thermal-centrifuge.dumpToggle = false;
