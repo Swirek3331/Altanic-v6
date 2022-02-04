083eb2ca891066f1282ec45293229a8cd3b6e368
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-04 16:47:38 +0100

    Update and rename Attributes.js to attributes.js

--- scripts/attributes.js
+++ scripts/attributes.js
@@ -4,4 +4,4 @@ Block.charr.attributes = set(Attribute.stone, 1.2f);
 Block.dacite.attributes = set(Attribute.stone, 1f);
 Block.basalt.attributes = set(Attribute.stone, 1.5f);
 Block.shale.attributes = set(Attribute.stone, 1.75f);
-//I don't think i will be work
+//I don't think it will be work
