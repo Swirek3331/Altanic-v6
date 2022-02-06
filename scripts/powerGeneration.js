eee822a9051a01e56e92562658ad9a54f5f10e6d
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-06 17:40:10 +0100

    Update powerGeneration.js

--- scripts/powerGeneration.js
+++ scripts/powerGeneration.js
@@ -76,7 +76,7 @@ hasLiquids
 hasPower
 */
 //using example without .json file. I don't recommand this way because you can't use mod item as requirements.
-nuclearReactor.localizedName = "Nuclear reactor;
+nuclearReactor.localizedName = "Nuclear reactor"
 nuclearReactor.description = "Nuclear reactor require radioactive materials, produce more power than thorium reactor but requires more coolant and produce waste"
 furnace.itemCapacity = 20;
 nuclearReactor.liquidCapacity = 180;
