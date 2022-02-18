fcd05105366237c3ebe4fc6fe83daebc3dc35e28
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-02-18 07:36:47 +0100

    Delete attributes.js

--- scripts/attributes.js
+++
@@ -1,7 +0,0 @@
-Block.stone.attributes = set(Attribute.stone, 1f);
-Block.craters.attributes = set(Attribute.stone, 1f);
-Block.charr.attributes = set(Attribute.stone, 1.2f);
-Block.dacite.attributes = set(Attribute.stone, 1f);
-Block.basalt.attributes = set(Attribute.stone, 1.5f);
-Block.shale.attributes = set(Attribute.stone, 1.75f);
-//I don't think it will be work
