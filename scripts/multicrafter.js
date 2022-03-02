218585836777afebaa76fe51ebe81139abe17601
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-03-02 16:21:16 +0100

    Update multicrafter.js

--- scripts/multicrafter.js
+++ scripts/multicrafter.js
@@ -642,7 +642,7 @@ const assembler = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCr
     {
         input: {
             items: ["altanic-plutonium/10", "altanic-stal/50"],
-            liquids: ["oil/60"],
+            liquids: ["altanic-turbofuel/10"],
             power: 5,
         },
         output: {
@@ -815,58 +815,6 @@ melter.health = 500;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 melter.dumpToggle = true;
-
-const assembler = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "assembler", [
-    /*default form for each recipes. You can change values.
-    {
-        input:{
-            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
-            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
-            power:0,
-        },
-        output:{
-            items:[],
-            liquids:[],
-            power:0,
-        },
-        craftTime:80,
-    },*/
-], {
-    /*you can customize block here. ex) load()*/
-},
-    /*this is Object constructor. This way is much better than literal way{a:123}
-    you can replace this with {} if you don't want to modify entity*/
-    function Extra() {
-        /*you can use customUpdate=function(){}. this function excuted before update()
-        also this.draw=function(){}
-        you can customize entity here.
-        ex)
-        this._myProp=0;
-        this.getMyProp=function(){
-            return this._myProp;
-        };
-        this.setMyProp=function(a){
-            this._myProp=a;
-        };*/
-    });
-/*
-YOU MUST NOT MODIFY VALUE OF THESE
-configurable
-outputsPower
-hasItems
-hasLiquids
-hasPower
-*/
-//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
-assembler.localizedName = "Assembler";
-assembler.description = "Assembler";
-assembler.itemCapacity = 20;
-melter.liquidCapacity = 60;
-assembler.size = 2;
-assembler.health = 500;
-/*true: dump items and liquids of output according to button
-false: dump items and liquids of output unconditionally*/
-assembler.dumpToggle = true;
 
 //compressor
 
