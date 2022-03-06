4e816a8ccccb3fc406c50d0c0568a4372322d047
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-03-06 10:17:27 +0100

    Update multicrafter.js

--- scripts/multicrafter.js
+++ scripts/multicrafter.js
@@ -978,8 +978,65 @@ compressor.health = 500;
 false: dump items and liquids of output unconditionally*/
 compressor.dumpToggle = true;
 
-//fermenter
-
-//rafinery
-
-//mixer
+const mixer = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "mixer", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["titanium/1"],
+            liquids: ["water/12"],
+            power: 1.5
+        },
+        output: {
+            liquids: ["cryofliud/12"]
+        },
+        craftTime: 60
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
+mixer.localizedName = "Compressor";
+mixer.description = "Compressor";
+mixer.itemCapacity = 20;
+mixer.liquidCapacity = 60;
+mixer.size = 2;
+mixer.health = 500;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+mixer.dumpToggle = true;
