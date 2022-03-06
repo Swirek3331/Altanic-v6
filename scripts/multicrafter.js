7a8ae77f98aa53425c63741162a40dec53e582c2
Karol Sutkowski <82308691+Swirek3331@users.noreply.github.com>
2022-03-06 09:56:54 +0100

    Update multicrafter.js

--- scripts/multicrafter.js
+++ scripts/multicrafter.js
@@ -751,7 +751,7 @@ oreWasher.dumpToggle = true;
 
 //centrifuge
 
-const geothermalMelter = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "geothermal-melter", [
+const thermalMelter = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "thermal-melter", [
     /*default form for each recipes. You can change values.
     {
         input:{
@@ -823,15 +823,15 @@ hasLiquids
 hasPower
 */
 //using example without .json file. I don't recommand this way because you can't use mod item as requirements.
-geothermalMelter.localizedName = "melter";
-geothermalMelter.description = "melter";
-geothermalMelter.itemCapacity = 20;
-geothermalMelter.liquidCapacity = 60;
-geothermalMelter.size = 2;
-geothermalMelter.health = 500;
+thermalMelter.localizedName = "melter";
+thermalMelter.description = "melter";
+thermalMelter.itemCapacity = 20;
+thermalMelter.liquidCapacity = 60;
+thermalMelter.size = 2;
+thermalMelter.health = 500;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
-geothermalMelter.dumpToggle = true;
+thermalMelter.dumpToggle = true;
 
 const industrialPress = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "industrial-press", [
     /*default form for each recipes. You can change values.
@@ -866,7 +866,7 @@ const industrialPress = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.Gen
         output: {
             items: ["graphite/2"],
         },
-        craftTime: 60
+        craftTime: 90
     },
 ], {
     /*you can customize block here. ex) load()*/
@@ -904,7 +904,79 @@ industrialPress.health = 500;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 industrialPress.dumpToggle = true;
-//compressor
+
+const compressor = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "compressor", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["coal/4"],
+            power: 0.5
+        },
+        output: {
+            items: ["graphite/4"],
+        },
+        craftTime: 90
+    },
+    {
+        input: {
+            items: ["altanic-cokecoal/2"],
+            liquids: ["water/6"],
+            power: 0.8,
+        },
+        output: {
+            items: ["graphite/8"],
+        },
+        craftTime: 60
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .json file. I don't recommand this way because you can't use mod item as requirements.
+compressor.localizedName = "Compressor";
+compressor.description = "Compressor";
+compressor.itemCapacity = 20;
+compressor.liquidCapacity = 60;
+compressor.size = 2;
+compressor.health = 500;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+compressor.dumpToggle = true;
 
 //fermenter
 
