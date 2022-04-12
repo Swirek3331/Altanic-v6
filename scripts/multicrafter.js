c60930709ccaf3c676f896a901a66b4da43546bf
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-04-12 11:41:02 +0100

    Kapsułownik

--- scripts/multicrafter.js
+++ scripts/multicrafter.js
@@ -1633,3 +1633,430 @@ radioisotopeThermoelectricBoiler.health = 250;
 /*true: dump items and liquids of output according to button
 false: dump items and liquids of output unconditionally*/
 radioisotopeThermoelectricBoiler.dumpToggle = false;
+
+const canner = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "canner", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-biofuel/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-biofuel-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-biogas/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-biogas-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-coil/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-coil-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["cryofluid/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-cryofluid-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-ethanol/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-ethanol-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-gas/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-gas-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-gasoline/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-gasoline-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-heavy-oil/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-heavy-oil-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-lava/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-lava-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-lubricant/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-lubricant-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-naphta/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-naphta-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["oil/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-oil-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-plant-oil/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-plant-oil-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-refined-fuel/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-refined-fuel-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["slag/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-slag-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-steam/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-steam-can/1"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["water/12"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-water-can/1"],
+        },
+        craftTime: 30
+    },
+    //unloading
+    {
+        input: {
+            items: ["altanic-biofuel-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-biofuel/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-biogas-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-biogas/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-coil-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-coil/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-cryofluid-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["cryofluid/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-ethanol-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-ethanol/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-gas-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-gas/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-gasoline-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-gasoline/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-heavy-oil-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-heavy-oil/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-lava-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-lava/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-lubricant-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-lubricant/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-naphta-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-naphta/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-oil-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["oil/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-plant-oil-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-plant-oil/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-refined-fuel-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-refined-fuel/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-slag-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["slag/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-steam-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["altanic-steam/12"],
+        },
+        craftTime: 30
+    },
+    {
+        input: {
+            items: ["altanic-water-can/1"],
+            power: 0.1,
+        },
+        output: {
+            items: ["altanic-liquid-can/1"],
+            liquids: ["water/12"],
+        },
+        craftTime: 30
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .hjson file. I don't recommand this way because you can't use mod item as requirements.
+canner.localizedName = "Canner";
+canner.description = "Canner";
+canner.itemCapacity = 4;
+canner.liquidCapacity = 24;
+canner.size = 1;
+canner.health = 100;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+canner.dumpToggle = true;
