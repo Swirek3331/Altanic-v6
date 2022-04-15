403882ffd2b06a34eeb35bc0f81e428f55138564
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-04-15 11:22:06 +0100

    pirolizownia

--- scripts/multicrafter.js
+++ scripts/multicrafter.js
@@ -113,6 +113,64 @@ furnace.health = 250;
 false: dump items and liquids of output unconditionally*/
 furnace.dumpToggle = true;
 
+const pyrolysisPlant = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "pyrolysisPlant", [
+    {
+        input: {
+            items: ["altanic-wood/5"],
+            power: 1
+        },
+        output: {
+            items: ["coal/1"],
+            liquids: ["creosote-oil/6"],
+        },
+        craftTime: 300
+    },
+    {
+        input: {
+            items: ["coal/5"],
+            power: 2,
+        },
+        output: {
+            items: ["altanic-cokecoal/5"],
+            liquids: ["creosote-oil/12"],
+        },
+        craftTime: 120
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .hjson file. I don't recommand this way because you can't use mod item as requirements.
+pyrolysisPlant.localizedName = "Pyrolysis Plant";
+pyrolysisPlant.description = "Pyrolysis Plant";
+pyrolysisPlant.itemCapacity = 20;
+pyrolysisPlant.size = 2;
+pyrolysisPlant.health = 250;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+pyrolysisPlant.dumpToggle = true;
 
 
 const inductionFurnace = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "induction-furnace", [
