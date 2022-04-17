958ced4b2bcf12c0bc418a827231eaf5a4dd73d2
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-04-17 12:59:34 +0100

    w przyszłości się przyda

---
+++ scripts/bullets/nukeBullet.js
@@ -0,0 +1,9 @@
+const nukeBulletType = extend(BasicBulletType);
+
+nukeBulletType.speed = 2;
+nukeBulletType.damage = 2500;
+nukeBulletType.splashDamageRadius = 30;
+nukeBulletType.splashDamage = 30 * (nukeBulletType.splashDamageRadius / 100);
+nukeBulletType.nukeBulletammoMultiplier = 1;
+nukeBulletType.hitEffect = reactorExplosion;
+nukeBulletType.despawnEffect = shootBigSmoke;
