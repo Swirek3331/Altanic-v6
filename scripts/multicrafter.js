78ec07933e0c1bbf09a6cf0c83fb450e2d427218
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-04-20 16:58:11 +0100

    coś i coś

--- scripts/multicrafter.js
+++ scripts/multicrafter.js
@@ -1458,6 +1458,69 @@ mixer.health = 500;
 false: dump items and liquids of output unconditionally*/
 mixer.dumpToggle = true;
 
+-const oreWasher = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "ore-washer", [
+    /*default form for each recipes. You can change values.
+    {
+        input:{
+            items:[],     Modded Item:  "mod-name-item-name/amount", Vanilla Item: "item-name/amount"
+            liquids:[],   Modded Liquid:  "mod-name-liquid-name/amount",  Vanilla liquid: "liquid-name/amount"
+            power:0,
+        },
+        output:{
+            items:[],
+            liquids:[],
+            power:0,
+        },
+        craftTime:80,
+    },*/
+    {
+        input: {
+            items: ["altanic-ore-vein/1"],
+            liquids: ["water/12"],
+            power: 2.5,
+        },
+        output: {
+            items: ["altanic-purified-ore-vein/1", "altanic-stone/15"],
+        },
+        craftTime: 120
+    },
+], {
+    /*you can customize block here. ex) load()*/
+},
+    /*this is Object constructor. This way is much better than literal way{a:123}
+    you can replace this with {} if you don't want to modify entity*/
+    function Extra() {
+        /*you can use customUpdate=function(){}. this function excuted before update()
+        also this.draw=function(){}
+        you can customize entity here.
+        ex)
+        this._myProp=0;
+        this.getMyProp=function(){
+            return this._myProp;
+        };
+        this.setMyProp=function(a){
+            this._myProp=a;
+        };*/
+    });
+/*
+YOU MUST NOT MODIFY VALUE OF THESE
+configurable
+outputsPower
+hasItems
+hasLiquids
+hasPower
+*/
+//using example without .hjson file. I don't recommand this way because you can't use mod item as requirements.
+oreWasher.localizedName = "Ore washer";
+oreWasher.description = "Ore washer";
+oreWasher.itemCapacity = 20;
+oreWasher.liquidCapacity = 60;
+oreWasher.size = 2;
+oreWasher.health = 500;
+/*true: dump items and liquids of output according to button
+false: dump items and liquids of output unconditionally*/
+oreWasher.dumpToggle = true;
+
 const thermalMelter = multiLib.MultiCrafter(GenericCrafter, GenericCrafter.GenericCrafterBuild, "thermal-melter", [
     /*default form for each recipes. You can change values.
     {
